a5f6d0f34ebb459bc58a747df70b016819d8b0cf
guiTortelli <100200595+guiTortelli@users.noreply.github.com>
2022-07-15 10:44:23 +0100

    TesteCV
    testeCV

---
+++ APICandidatos/APICandidatos/Model/CV.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APICandidatos.Model
+{
+    public class CV
+    {
+        [Key]
+        public int IdCV { get; set; }
+        public string? Nome { get; set; }
+        public string? Localizacao { get; set; }
+        public string? Educacao { get; set; }
+        public string? ExpProfissional { get; set; }
+        public string? Competencias { get; set; }
+        public string? Interesses { get; set; }
+    }
+}
